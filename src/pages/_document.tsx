b8586501fd2385279dbf2121a2d8b1d6f9d6ecfa
Doodream <rhomo17@naver.com>
2021-06-22 17:58:55 +0900

    Add : src/pages/_document.tsx : SSR 렌더링 스타일 컴포넌트 적용

---
+++ src/pages/_document.tsx
@@ -0,0 +1,75 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+import React, { ReactElement } from "react";
+import { ServerStyleSheet, createGlobalStyle } from "styled-components";
+// _document.js는 index.html을 꾸며주는거다라고 생각하면 된다.
+const GlobalStyles = createGlobalStyle`
+  body{
+    background: white;
+    font-family: 'Source Sans Pro', sans-serif;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+  }
+`;
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(
+              <>
+                <GlobalStyles />
+                <App {...props} />
+              </>
+            ),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head>
+          {/* <meta name="viewport" content="viewport-fit=cover" /> */}
+          {/* <meta name="viewport" content="width=device-width" /> */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <div id="root">
+            <Main />
+            <NextScript />
+          </div>
+        </body>
+      </Html>
+    );
+  }
+}
