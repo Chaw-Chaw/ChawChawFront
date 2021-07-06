4f064fb06b690ddb7cc32f9d8b641a8a57e80292
Doodream <rhomo17@naver.com>
2021-07-07 00:11:27 +0900

    Modify : src/components/common/Header.tsx : Header 배경색 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
-import styled from "styled-components";
-import { Button, Logo } from ".";
+import styled, { ThemeContext } from "styled-components";
+import { Button, Logo, ThemeToggle } from ".";
 
 const Header: React.FC = () => {
+  const { id, setTheme } = useContext(ThemeContext);
   return (
     <HeaderWrapper>
       <Logo />
-      <Button>Login</Button>
-      <ButtonJustStart>Just Start</ButtonJustStart>
+      <ThemeToggleBox>
+        <ThemeToggle isActive={id === "dark"} onToggle={setTheme} />
+      </ThemeToggleBox>
+      <ButtonLogin>Login</ButtonLogin>
     </HeaderWrapper>
   );
 };
 
+const ThemeToggleBox = styled.div`
+  margin: 1rem;
+`;
 const HeaderWrapper = styled.div`
+  background-color: ${(props) => props.theme.bodyBackgroundColor};
   align-items: center;
   display: flex;
   justify-content: center;
@@ -28,6 +35,11 @@ const HeaderWrapper = styled.div`
     bottom: 0%;
     top: initial;
   }
+`;
+const ButtonLogin = styled(Button)`
+  width: 5rem;
+  height: 2rem;
+  font-size: 1.2rem;
 `;
 
 const ButtonJustStart = styled(Button)`
