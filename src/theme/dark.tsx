c6d0f4dfbb55f106d52acdaf7d4165332939ded3
Doodream <rhomo17@naver.com>
2021-07-07 00:09:59 +0900

    Modify : src/theme/dark.tsx, src/theme/light.tsx : 테마 색 수정

--- src/theme/dark.tsx
+++ src/theme/dark.tsx
@@ -1,7 +1,7 @@
 const theme = {
   id: "dark",
   primaryColor: "#FF8A00",
-  secondaryColor: "midnightblue",
+  secondaryColor: "#D6D6D6",
   bodyBackgroundColor: "#4b4b4b",
   bodyFontColor: "white",
 };
--- src/theme/light.tsx
+++ src/theme/light.tsx
@@ -1,7 +1,7 @@
 const theme = {
   id: "light",
   primaryColor: "#FF8A00",
-  secondaryColor: "midnightblue",
+  secondaryColor: "#D6D6D6",
   bodyBackgroundColor: "white",
   bodyFontColor: "black",
 };
