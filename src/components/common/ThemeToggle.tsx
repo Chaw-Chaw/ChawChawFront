282c0f6884d929e6814924514859783b2a80f152
Doodream <rhomo17@naver.com>
2021-07-07 00:09:18 +0900

    Add : src/components/common/ThemeToggle.tsx : ThemeToogle 컴포넌트 추가

--- src/components/common/ThemeToggle.tsx
+++ src/components/common/ThemeToggle.tsx
@@ -1,3 +1,46 @@
-import React, { Children } from "react";
+import React, { Children, ReactNode, useContext } from "react";
 import Link from "next/link";
-import styled, { css } from "styled-components";
+import styled, { ThemeContext } from "styled-components";
+
+interface ThemeToggleProps {
+  isActive?: boolean;
+  onToggle?: Function;
+}
+
+const ToggleWrapper = styled.div`
+  width: 50px;
+  min-width: 50px;
+  height: 25px;
+  border-radius: 25px;
+  cursor: pointer;
+  display: flex;
+  background: ${(props) =>
+    props.theme.id === "light"
+      ? props.theme.secondaryColor
+      : props.theme.primaryColor};
+  align-items: center;
+`;
+
+const Notch = styled.div<{ isActive: boolean }>`
+  height: 21px;
+  width: 21px;
+  margin-top: 1px;
+  background: white;
+  border-radius: 50%;
+  transition: transform 0.1s linear;
+  transform: translate(${(p) => (p.isActive ? "28px" : "1px")});
+  text-align: center;
+  background: ${(props) =>
+    props.theme.id === "light" ? props.theme.primaryColor : "white"};
+`;
+
+const ThemeToggle: React.FC<ThemeToggleProps> = (props) => {
+  const { id } = useContext(ThemeContext);
+  return (
+    <ToggleWrapper onClick={props.onToggle}>
+      <Notch isActive={props.isActive}></Notch>
+    </ToggleWrapper>
+  );
+};
+
+export { ThemeToggle };
