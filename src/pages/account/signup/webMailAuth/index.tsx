d5bca2bbf6a96dacc51037016239aa67cb5bbe00
Doodream <rhomo17@naver.com>
2021-07-15 09:26:52 +0900

    Add : 웹 메일 인증 페이지 생성

---
+++ src/pages/account/signup/webMailAuth/index.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  Layout,
+  Header,
+  Input,
+  PasswordInput,
+  Label,
+  Button,
+} from "../../../../components/common/";
+import { AccountContainer } from "../../components/AccountContainer";
+import { LoginOrder } from "../../components/LoginOrder";
+import styled from "styled-components";
+import Link from "next/link";
+
+const InputSection = styled.div`
+  width: 100%;
+  margin: 20px 0;
+`;
+const ButtonSection = styled.div<{ marginRight?: string; marginLeft?: string }>`
+  margin-right: ${(props) => (props.marginRight ? props.marginRight : "0px")};
+  margin-left: ${(props) => (props.marginLeft ? props.marginLeft : "0px")};
+  width: 100%;
+`;
+
+const MovePageButtonSection = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  display: flex;
+`;
+
+export default function WebMailAuth() {
+  return (
+    <Layout>
+      <Header type="signup" />
+      <AccountContainer
+        title="ChawChaw에`오신 것을 환영 해요."
+        subtitle="현재 재학중인 대학교의 웹메일을 입력해주세요.`웹 메일로 인증번호가 발송됩니다."
+      >
+        <LoginOrder activeType="1" />
+        <InputSection>
+          <Label htmlFor="webmail" tag="필수">
+            웹 메일
+          </Label>
+          <Input name="webmail" />
+        </InputSection>
+        <ButtonSection>
+          <Button width="100%" height="2rem" fontSize="1rem">
+            발송하기
+          </Button>
+        </ButtonSection>
+        <InputSection>
+          <Label htmlFor="authNumber" tag="필수">
+            인증번호
+          </Label>
+          <Input name="authNumber" />
+        </InputSection>
+        <MovePageButtonSection>
+          <ButtonSection marginRight="20px">
+            <Link href="/">
+              <a>
+                <Button secondary width="100%" height="4rem" fontSize="1rem">
+                  페이지 소개
+                </Button>
+              </a>
+            </Link>
+          </ButtonSection>
+          <ButtonSection marginLeft="20px">
+            <Link href="/account/signup">
+              <a>
+                <Button width="100%" height="4rem" fontSize="1rem">
+                  회원 가입
+                </Button>
+              </a>
+            </Link>
+          </ButtonSection>
+        </MovePageButtonSection>
+      </AccountContainer>
+    </Layout>
+  );
+}
