60bd68058640cce37714fe3191f284efd0459398
Doodream <rhomo17@naver.com>
2021-07-15 09:20:08 +0900

    Add : Input Label Component 생성

---
+++ src/components/common/Label.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+interface LabelProps {
+  tag?: string;
+  htmlFor?: string;
+}
+const MainLabel = styled.label`
+  font-size: 1.2rem;
+  font-weight: 600;
+`;
+
+const Tag = styled.span`
+  font-size: 1rem;
+  color: ${(props) => props.theme.primaryColor};
+  font-weight: 500;
+`;
+
+const Box = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Label: React.FC<LabelProps> = (props) => {
+  return (
+    <Box>
+      <MainLabel htmlFor={props.htmlFor}>{props.children}</MainLabel>
+      {props.tag ? <Tag>({props.tag})</Tag> : null}
+    </Box>
+  );
+};
+
+export { Label };
