0715fddd39c987ce314897d590b56af217e62c7e
Doodream <rhomo17@naver.com>
2021-07-15 09:21:09 +0900

    Modify : button box shadow 변경

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,18 +2,11 @@ import React, { Children } from "react";
 import Link from "next/link";
 import styled, { css } from "styled-components";
 
-// const Button: React.FC = ({ children }) => {
-//   return <StyleButton>{children}</StyleButton>;
-// };
-// interface Props extends <HTMLButtonElement> {
-//     secondary: boolean;
-// }
-
 interface ButtonProps {
   width?: string;
   height?: string;
-  primary?: string;
-  secondary?: string;
+  primary?: boolean;
+  secondary?: boolean;
   fontWeight?: string;
   fontSize?: string;
 }
@@ -28,15 +21,18 @@ const InitialButton = styled.button<ButtonProps>`
   border-radius: 20rem;
   background-color: ${(props) => {
     if (props.primary) return props.theme.primaryColor;
-    if (props.secondary) return "white";
+    if (props.secondary) return props.theme.bodyBackgroundColor;
     return props.theme.primaryColor;
   }};
+  /* border: ${(props) =>
+    props.secondary ? `1px solid ${props.theme.primaryColor}` : "none"}; */
+  border: 1px solid ${(props) => props.theme.primaryColor};
   width: ${(props) => (props.width ? props.width : "6rem")};
   height: ${(props) => (props.height ? props.height : "2rem")};
   font-weight: ${(props) => (props.fontWeight ? props.fontWeight : "600")};
-  font-size: ${(props) => (props.fontSize ? props.fontSize : "1.5rem")};
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "1rem")};
   cursor: pointer;
-  border: none;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.5);
   text-align: center;
   &:disabled {
     background-color: ${(props) => props.theme.bodyBackgroundColor};
