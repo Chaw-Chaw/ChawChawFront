1991c703c6e9a64068fbdcd3e6fd2feb5ef1df16
Doodream <rhomo17@naver.com>
2021-07-20 16:31:46 +0900

    Add : 로그인 프로세스 시 프로필 저장화면

--- src/pages/account/signup/profile/index.tsx
+++ src/pages/account/signup/profile/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import {
-  Layout,
-  Header,
-  Input,
-  Label,
-  Button,
-} from "../../../../components/common/";
+import { Layout, Button } from "../../../../components/common/";
 import AccountContainer from "../../components/AccountContainer";
 import LoginOrder from "../../components/LoginOrder";
 import styled from "styled-components";
 import Link from "next/link";
+import ProfileSection from "../../../../components/ProfileSection/ProfileSection";
 
-const InputSection = styled.div`
-  width: 100%;
-  margin: 10px 0;
-`;
 const ButtonSection = styled.div<{ marginRight?: string; marginLeft?: string }>`
   margin-right: ${(props) => (props.marginRight ? props.marginRight : "0px")};
   margin-left: ${(props) => (props.marginLeft ? props.marginLeft : "0px")};
@@ -23,20 +14,20 @@ const ButtonSection = styled.div<{ marginRight?: string; marginLeft?: string }>`
 
 const MovePageButtonSection = styled.div`
   margin-top: 20px;
-  width: 100%;
+  width: 450px;
   display: flex;
 `;
 
 export default function Profile() {
   return (
-    <Layout>
-      <Header type="profile" />
+    <Layout type="profile">
       <AccountContainer
         title="프로필 생성"
         subtitle="프로필을 완성해서` 자신의 매력을 포스팅 해보세요."
+        width="640px"
       >
         <LoginOrder activeType="3" />
-
+        <ProfileSection />
         <MovePageButtonSection>
           <ButtonSection marginRight="20px">
             <Link href="/account/signup">
