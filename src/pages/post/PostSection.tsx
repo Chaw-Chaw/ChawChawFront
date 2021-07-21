594ac945da395ed980f2cabae24c5932363b41a5
Doodream <rhomo17@naver.com>
2021-07-21 23:23:26 +0900

    babel 설정 수정

--- src/pages/post/PostSection.tsx
+++ src/pages/post/PostSection.tsx
@@ -15,25 +15,24 @@ const PostSectionBox = styled.div`
   } ;
 `;
 
-const examplePost = (
-  <PostCard viewCount={200} likeCount={200} pastDate={200}>
-    안녕 Army들 ~ 어? 예쁘다.
-    <br /> Hi~ 에이치아이~ BTS 언어를 배우고 싶어요 한국에 있는 아미들하고
-    언어교환해요 즐겁게 즐겁게! <br />
-    <br />
-    나랑 놀사람 얼른 여기로 붙어요!! 나는 한국인이구 영어랑 프랑스어를 배우고
-    싶어요! <br />
-    <br />
-    185에 마른 근육도 쩔구 노래랑 인기도 대박 많아요
-    <br /> 나랑 놀사람~ 유학생들이 bts V를 팔로워 하는게 얼마나 어려운데 이렇게
-    팬미팅 하는거지 어쩌구 저쩌구 <br />
-  </PostCard>
-);
 const PostSection: React.FC = (props) => {
   return (
     <PostSectionBox>
       {Array.from({ length: 17 }, () => 1).map((_, index) => {
-        return examplePost;
+        return (
+          <PostCard key={index} viewCount={200} likeCount={200} pastDate={200}>
+            안녕 Army들 ~ 어? 예쁘다.
+            <br /> Hi~ 에이치아이~ BTS 언어를 배우고 싶어요 한국에 있는
+            아미들하고 언어교환해요 즐겁게 즐겁게! <br />
+            <br />
+            나랑 놀사람 얼른 여기로 붙어요!! 나는 한국인이구 영어랑 프랑스어를
+            배우고 싶어요! <br />
+            <br />
+            185에 마른 근육도 쩔구 노래랑 인기도 대박 많아요
+            <br /> 나랑 놀사람~ 유학생들이 bts V를 팔로워 하는게 얼마나 어려운데
+            이렇게 팬미팅 하는거지 어쩌구 저쩌구 <br />
+          </PostCard>
+        );
       })}
     </PostSectionBox>
   );
