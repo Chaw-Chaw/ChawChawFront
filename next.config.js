96ade29cbca88ca098921598165a8f8e12131120
Doodream <rhomo17@naver.com>
2021-07-26 17:15:59 +0900

    Add : axios 설정 , proxy 설정

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://mylifeforcoding.com/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
