a02cb7f624fe36b28dc19aed1d4664bec7043119
Doodream <rhomo17@naver.com>
2021-07-27 17:19:27 +0900

    Add : Social Button 모양 수정 및 컴포넌트 정리

---
+++ src/pages/account/login/SocialSection.tsx
@@ -0,0 +1,92 @@
+import React, { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import CSS from "csstype";
+import Link from "next/link";
+import { AuthContext } from "../../../store/AuthContext";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { FaFacebookF } from "react-icons/fa";
+
+const ButtonSection = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const SocialContainer = styled.div`
+  border-top: 1px solid ${(props) => props.theme.secondaryColor};
+  margin-top: 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SocialButtonTitle = styled.h2`
+  margin: 10px auto 30px auto;
+  color: ${(props) =>
+    props.theme.id === "light" ? "rgba(0, 0, 0, 0.3)" : "white"};
+  font-size: 1rem;
+`;
+
+const KakaoLogin = styled.button`
+  cursor: pointer;
+  width: 60px;
+  height: 60px;
+  border: none;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.5);
+  border-radius: 100%;
+  margin: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fae100;
+  color: #3c1d1e;
+  font-size: 2.5rem;
+`;
+const FacebookLogin = styled(KakaoLogin)`
+  color: white;
+  background: #3d5a97;
+`;
+
+const SocialSection: React.FC = (props) => {
+  const router = useRouter();
+  const callKakaoLogin = () => {
+    router.push({
+      pathname: "https://kauth.kakao.com/oauth/authorize",
+      query: {
+        response_type: "code",
+        client_id: "0c867f53d75cc0e2a7932427b908806b",
+        redirect_uri: "http://localhost:3000/account/oauth",
+      },
+    });
+  };
+  return (
+    <SocialContainer>
+      <SocialButtonTitle>소셜계정으로 로그인 | 회원가입</SocialButtonTitle>
+      <ButtonSection>
+        <KakaoLogin onClick={callKakaoLogin}>
+          <RiKakaoTalkFill />
+        </KakaoLogin>
+        <FacebookLogin
+        //   style={styleFacebookLogin}
+        //   appId="1235018336951383"
+        //   onSuccess={(response) => {
+        //     console.log(response, "Login Success!");
+        //     //console.log("id: ", response.id);
+        //   }}
+        //   onFail={(error) => {
+        //     console.log("Login Failed!");
+        //     console.log("status: ", error.status);
+        //   }}
+        //   onProfileSuccess={(response) => {
+        //     console.log(response, "Get Profile Success!");
+        //   }}
+        >
+          <FaFacebookF />
+        </FacebookLogin>
+      </ButtonSection>
+    </SocialContainer>
+  );
+};
+
+export default SocialSection;
