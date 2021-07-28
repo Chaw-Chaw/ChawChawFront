1af4a75f2bd8e84c1ee8f3062dfdc08751e62672
Doodream <rhomo17@naver.com>
2021-07-28 19:29:26 +0900

    Modify : nextjs img lazyloading 추가

--- src/components/home/ImageSection.tsx
+++ src/components/home/ImageSection.tsx
@@ -11,15 +11,12 @@ const ImageWrapper = styled.div`
   width: 100%;
   height: 100%;
   margin: 50px 50px;
-  .image {
-    object-fit: contain;
-  }
 `;
 
 const ImageSection: React.FC<ImageSectionProps> = (props) => {
   return (
     <ImageWrapper>
-      <Image src={props.src} alt={props.alt} className="image"></Image>
+      <Image src={props.src} alt={props.alt} objectFit="contain"></Image>
     </ImageWrapper>
   );
 };
