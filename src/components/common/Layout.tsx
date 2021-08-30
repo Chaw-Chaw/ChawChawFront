4352474f6adc95d4dacd946256a4b2c41f504f65
Doodream <rhomo17@naver.com>
2021-08-30 11:47:28 +0900

    Update : next-seo를 이용한 meta tag 적용

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -3,19 +3,47 @@ import styled from "styled-components";
 import Header from "./Header";
 import Head from "next/head";
 import { AuthContext } from "../../store/AuthContext";
+import { NextSeo } from "next-seo";
 
 const Layout: React.FC<{ type?: string }> = (props) => {
   return (
     <>
-      {/* <Head>
-        <title>ChawChaw</title>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/Layout/chaw.png"
+      <Head>
+        <NextSeo
+          title={"ChawChaw 언어를 교환합시다.🗣"}
+          description={"대학내 교환학생 언어교환 채팅 어플리케이션입니다."}
+          canonical="https://www.chawchaw.vercel.app"
+          openGraph={{
+            type: "website",
+            locale: "en_IE",
+            title: "ChawChaw 언어를 교환합시다.🗣",
+            description: "대학내 교환학생 언어교환 채팅 어플리케이션입니다.",
+            images: [
+              {
+                url: "https://i.ibb.co/m0NY7yQ/image.jpg",
+                width: 800,
+                height: 600,
+                alt: "ChawChaw 소개 이미지",
+              },
+            ],
+            url: "https://www.chawchaw.vercel.app",
+            site_name: "ChawChaw",
+          }}
+          twitter={{
+            handle: "@chawchawTwitter",
+            site: "chawchaw.vercel.app",
+            cardType: "summary",
+          }}
+          additionalLinkTags={[
+            {
+              type: "image/png",
+              sizes: "32x32",
+              href: "/Layout/chaw.png",
+              rel: "icon",
+            },
+          ]}
         />
-      </Head> */}
+      </Head>
       <Header type={props.type} />
       <Inner>{props.children}</Inner>
     </>
