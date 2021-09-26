f0a5f49f415c020170e2f895fa1e89a10d67853a
Doodream <rhomo17@naver.com>
2021-09-26 21:50:47 +0900

    Add : 회원정보 대학추가

---
+++ src/components/setting/SettingUserUniversity.tsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import { AuthContext } from "../../store/AuthContext";
+import { Input, ListItem } from "../common";
+
+const SettingUserUniversity: React.FC = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user.school, "userSchool");
+  return (
+    <ListItem
+      title="소속대학"
+      description="해당 대학 웹메일로 인증한 대학소속입니다."
+    >
+      <UserUniversity disabled value={user.school} />
+    </ListItem>
+  );
+};
+
+export { SettingUserUniversity };
+
+const UserUniversity = styled(Input)`
+  width: 40%;
+  @media (max-width: 768px) {
+    margin-top: 10px;
+  }
+`;
